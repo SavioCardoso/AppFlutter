41fb2d3107ab32d51678c9ee4222bd022fe4bd1c
Savio Cardoso <dev52>
2024-04-05 23:29:31 -0300

    Página de Login e Cadastro de Usuário

---
+++ flutter_application_1/lib/CampoTexto.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class CampoTexto extends StatefulWidget {
+  @override
+  _CampoTextoState createState()=> _CampoTextoState();
+}
+class _CampoTextoState extends State<CampoTexto> {
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
